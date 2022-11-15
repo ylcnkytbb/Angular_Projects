77d17611e01687a811293b6988a4b935d4508c04
ylcnkytbb <tugbayalcinkaya3437@gmail.com>
2022-11-15 21:03:21 +0300

    angular_todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'; //TodoComponenti oluşturduğumuzda bunlar otomatik geldi
 import { Model } from '../model';
+import { TodoItem } from '../todoitem';
 
 
 //artık app componenti silebiliriz
@@ -13,10 +14,15 @@ export class TodoComponent  {
    //message = ""; two way bindinde buna gerek yok 
 
   displayAll: boolean = true;
+  inputText: string = ""; // inputa herhangi bir şey yazmadığımızda add butonunun pasif olmasını sağlama işlemi
+  items = []; //görev listesinin tutulacağı dizi
 
-  constructor() { }
+  constructor() {
+    this.model.items = this.getItemsFromLS();
+   }
 
   model = new Model(); //model.tsden geldi
+
 
  // private name: string = "Tuğba";  //ilerleyen  aşamalarda isim bilgisini veritabanından alabilirim
   
@@ -51,12 +57,80 @@ export class TodoComponent  {
    
   }
 
-  addItem(value: string) {
-    if(value!="") {
-      this.model.items.push ({ description: value, action: false });
+  // addItem() {
+  //   if(this.inputText!="") {
+  //     this.model.items.push ({ description: this.inputText, action: false });
+  //     this.inputText = "";
+  //   } else{
+  //     alert("Bilgi giriniz")
+  //   }
+    
+  // }
+ 
+  //*******************Local Storage******************
+
+   addItem() {
+    if(this.inputText!="") {
+      let data = {description: this.inputText, action: false};
+      this.model.items.push(data);
+      
+      let items = this.getItemsFromLS();
+      items.push(data);
+      localStorage.setItem("items", JSON.stringify(items));  //veri saklayabilmek için
+      this.inputText = "";
     } else{
       alert("Bilgi giriniz")
     }
     
   }
+
+  // deleteItem(item: any) {
+  //   for (let i=0 ; i < this.items.length; i++){
+  //     if (item == this.items[i]){
+  //       this.items.splice(i, 1)
+  //     }
+  //   }
+  // }
+
+  getItemsFromLS() {
+     let items: TodoItem[] = []  //TİP ATAMASI YAPTIK
+
+     let value = localStorage.getItem("items");
+     if(value != null ) {
+      items = JSON.parse(value);
+     }
+
+     return items;
+  }
+
+  onActionChanged(item: TodoItem) {
+   
+    let items = this.getItemsFromLS();
+
+    localStorage.clear();
+
+    items.forEach(i => {
+      if(i.description == item.description) {
+        i.action == item.action;
+      }
+    });
+
+    localStorage.setItem("items", JSON.stringify(items));
+
+  }
+
+
+  displayCount() {
+
+    return this.model.items.filter(i => i.action).length; //tamamlanan görevlerin sayısını belirtmek için
+  }
+
+  getBtnClasses() {
+    return {
+      'disabled': this.inputText.length==0,
+      'btn-secondary':this.inputText.length==0,
+      'btn-primary':this.inputText.length>0
+    }
+  }
+
 }
